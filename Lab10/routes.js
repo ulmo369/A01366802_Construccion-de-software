482cf8602cf0384b048210f601dcf17a603e36cf
a01366802 <e1000lio2000@gmail.com>
2021-02-25 15:44:19 -0600

    correcciones lab10

--- Lab10/routes.js
+++ Lab10/routes.js
@@ -9,6 +9,9 @@ const requesthandler = (request, response) => {
         response.write("<html>");
         response.write("<head><title> Holaaa </title></head>");
         response.write("<body><h1> Holaaaaaa  </h1></body>");
+        response.write('<a target="_blank" href="http://localhost:3000/Buenas">localhost:3000/Buenas</a><br>')
+        response.write('<a target="_blank" href="http://localhost:3000/Texto">localhost:3000/Texto => aqui crear el archivo</a><br>')
+        response.write('<a target="_blank" href="http://localhost:3000/LoQueSea">localhost:3000/LoQueSea => esta no existe</a><br>')
         response.write("</html>")
         response.end();
     }
@@ -18,6 +21,9 @@ const requesthandler = (request, response) => {
         response.write("<html>");
         response.write("<head><title> Buenaaaas </title></head>");
         response.write("<body><h1> Buenaaaaas  </h1></body>");
+        response.write('<a target="_blank" href="http://localhost:3000/Hola">localhost:3000/Hola</a><br>')
+        response.write('<a target="_blank" href="http://localhost:3000/Texto">localhost:3000/Texto => aqui crear el archivo</a><br>')
+        response.write('<a target="_blank" href="http://localhost:3000/LoQueSea">localhost:3000/LoQueSea => esta no existe</a><br>')
         response.write("</html>")
         response.end();
     }
@@ -28,6 +34,9 @@ const requesthandler = (request, response) => {
         response.write("<head><title> Textoooo </title></head>");
         response.write("<body><h1> Que quieres escribir en el archivo de texto?  </h1>");
         response.write('<form action = "Texto" method = "POST"> <input type = "text" name = "texto"><input type = "submit" value = "Guardar texto"> </form>');
+        response.write('<a target="_blank" href="http://localhost:3000/Hola">localhost:3000/Hola</a><br>')
+        response.write('<a target="_blank" href="http://localhost:3000/Buenas">localhost:3000/Buenas</a><br>')
+        response.write('<a target="_blank" href="http://localhost:3000/LoQueSea">localhost:3000/LoQueSea => esta no existe</a><br>')
         response.write('</body>');
         response.write("</html>");
         response.end();
