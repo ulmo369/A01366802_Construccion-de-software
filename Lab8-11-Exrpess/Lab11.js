3fa842e705f7987409a76d7539c4f81083063858
a01366802 <e1000lio2000@gmail.com>
2021-02-25 23:09:53 -0600

    Lab11

--- Lab8-11-Exrpess/Lab11.js
+++ Lab8-11-Exrpess/Lab11.js
@@ -1,23 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+const filesystem = require('fs');
+
+var string = "";
 
 //Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
-app.use('/Personajes', (request, response, next) => {
-    response.send('<h1> Personajes </h1>'); 
+app.use('/Rutas/Ruta1', (request, response, next) => {
+    response.send('<h1> Ruta uno </h1>'); 
+});
+app.use('/Rutas/Ruta2', (request, response, next) => {
+    response.send('<h1> Ruta dos </h1>'); 
 });
 
-app.use('/', (request, response, next) => {
-    response.send('<h1> Main </h1>'); 
+app.get('/Rutas/Texto', (request, response, next) => {
+    response.send('<h1> Escribe tu texto </h1> <form action = "Texto" method = "POST"> <input type = "text" name = "texto"><input type = "submit" value = "Guardar texto"> </form>'); 
+
+});
+
+app.post('/Rutas/Texto', (request, response, next) => {
+    console.log(request.body.texto);
+    filesystem.writeFileSync('archivo.txt', request.body.texto);
+    response.redirect('/Rutas')
+    string = request.body.texto;
+    
+});
+
+app.use('/Rutas', (request, response, next) => {
+    console.log(string);
+    response.send('<h1> Aquí estará tu texto guardado </h1><br>' + string);
+
+});
+
+app.get('/', (request, response, next) => {
+    response.send('<h1> INICIO </h1>'); 
 });
 
 app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
+    response.statusCode = 404; 
+    response.send('<h1> Página no encontrada </h1>'); //Manda la respuesta
 });
+
 
 app.listen(3000);
